f7f42be3995376d7b7696d32bbb26483865751cc
Papana Dan-Cristian <38207553+DanPapana@users.noreply.github.com>
2020-04-08 01:38:26 +0300

    Add project files.

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using PAWEventive.ApplicationLogic.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PAWEventive.Models.Users;
+
+namespace PAWEventive.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Users/UserProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PAWEventive.ApplicationLogic.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static PAWEventive.ApplicationLogic.DataModel.Participation;
+
+namespace PAWEventive.Models.Users
+{
+    public class UserProfileViewModel
+    {
+        public User EventHost { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ContactDetails ContactDetails { get; set; }
+        public IEnumerable<Event> CreatedEvents { get;  set; }
+        public ParticipationType UserParticipationType { get; set; }
+    }
+}
